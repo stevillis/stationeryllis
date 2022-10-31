128855328693f4c7e1b48253f7fff07dd1870407
Stévillis Sousa <stevillis@hotmail.com>
2022-10-31 01:19:09 -0400

    feat: add CommissionParam to Django Admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,14 @@
+"""Admin related definitions."""
+
 from django.contrib import admin
 
-# Register your models here.
+from app.models import CommissionParam
+
+
+@admin.register(CommissionParam)
+class CommissionParamAdmin(admin.ModelAdmin):
+    """CommissionParam Model definitions for Django Admin."""
+    fields = ['day_of_the_week', 'min_percentage', 'max_percentage']
+    list_display = ['day_of_the_week', 'min_percentage', 'max_percentage']
+    list_filter = ['min_percentage', 'max_percentage']
+    search_fields = ['day_of_the_week']
