54a570358e031d581de39d4ef8591341c11a03e2
Stévillis Sousa <stevillis@hotmail.com>
2022-10-31 23:21:59 -0400

    feat: add customer model to django admin

--- app/admin.py
+++ app/admin.py
@@ -2,13 +2,21 @@
 
 from django.contrib import admin
 
-from app.models import CommissionParam
+from app.models import CommissionParam, Customer
 
 
 @admin.register(CommissionParam)
 class CommissionParamAdmin(admin.ModelAdmin):
     """CommissionParam Model definitions for Django Admin."""
-    fields = ['day_of_the_week', 'min_percentage', 'max_percentage']
-    list_display = ['day_of_the_week', 'min_percentage', 'max_percentage']
-    list_filter = ['min_percentage', 'max_percentage']
-    search_fields = ['day_of_the_week']
+    fields = ["day_of_the_week", "min_percentage", "max_percentage"]
+    list_display = ["day_of_the_week", "min_percentage", "max_percentage"]
+    list_filter = ["min_percentage", "max_percentage"]
+    search_fields = ["day_of_the_week"]
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    """Customer Model definitions for Django Admin."""
+    fields = ["name", "email", "phone"]
+    list_display = ["name", "email", "phone"]
+    search_fields = ["name", "email", "phone"]
