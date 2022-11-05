df38a276cf441057e2032b125c05105b5b131b1d
Stévillis Sousa <stevillis@hotmail.com>
2022-11-05 19:54:58 -0400

    refact: return customer id in CustomerSerializer

--- api/serializers/customer_serializer.py
+++ api/serializers/customer_serializer.py
@@ -10,4 +10,4 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         """Meta definitions"""
         model = Customer
-        fields = ('name', 'email', 'phone')
+        fields = ("id", "name", "email", "phone")
--- api/tests/serializers/test_customer_serializer.py
+++ api/tests/serializers/test_customer_serializer.py
@@ -13,47 +13,45 @@ class CustomerSerializerTestCase(TestCase):
     def setUp(self) -> None:
         """Set up common used values."""
         self.customer_attributes = {
-            'name': 'John Kent',
-            'email': 'johnkent@gmail.com',
-            'phone': '+1 415-387-2249'
+            "name": "John Kent",
+            "email": "johnkent@gmail.com",
+            "phone": "+1 415-387-2249"
         }
 
-        self.customer = Customer.objects.create(**self.customer_attributes)
+        self.customer = Customer.objects.create(
+            **self.customer_attributes)  # pylint: disable=no-member
         self.serializer = CustomerSerializer(instance=self.customer)
 
     def test_valid_data(self):
         """Test serializer with valid data."""
         data = self.serializer.data
 
-        with self.subTest("Test if the serializer has the exact attributes it is expected to"):
-            self.assertCountEqual(data.keys(), ['name', 'email', 'phone'])
-
-        with self.subTest("Test name field content has the expected data"):
-            self.assertEqual(data['name'], self.customer_attributes['name'])
-
-        with self.subTest("Test email field content has the expected data"):
-            self.assertEqual(data['email'], self.customer_attributes['email'])
-
-        with self.subTest("Test phone field content has the expected data"):
-            self.assertEqual(data['phone'], self.customer_attributes['phone'])
+        with self.subTest(
+            "CustomerSerializer should have the exact attributes and data as expected"
+        ):
+            self.assertCountEqual(
+                data.keys(), ["id", "name", "email", "phone"])
+            self.assertEqual(data["name"], self.customer_attributes["name"])
+            self.assertEqual(data["email"], self.customer_attributes["email"])
+            self.assertEqual(data["phone"], self.customer_attributes["phone"])
 
         with self.subTest("Test if duplicate email data raise exception"):
             serializer = CustomerSerializer(data=data)
+            is_valid = serializer.is_valid()
 
-            self.assertFalse(serializer.is_valid())
-            self.assertCountEqual(serializer.errors, ['email'])
-
-            email_errors = serializer.errors['email']
+            email_errors = serializer.errors["email"]
             error_code = email_errors[0].code
 
-            self.assertEqual(error_code, 'unique')
+            self.assertFalse(is_valid)
+            self.assertCountEqual(serializer.errors, ["email"])
+            self.assertEqual(error_code, "unique")
 
     def test_invalid_data(self):
         """Test invalid data."""
         serializer_data = {
-            'name': 'Mary Wayne',
-            'email': 'marywayne@gmail.com',
-            'phone': '0800 720 5356'
+            "name": "Mary Wayne",
+            "email": "marywayne@gmail.com",
+            "phone": "0800 720 5356"
         }
         with self.subTest("Test name field content with invalid data."):
             data = serializer_data.copy()
@@ -64,12 +62,12 @@ class CustomerSerializerTestCase(TestCase):
             ]
 
             for invalid_name in invalid_names:
-                data['name'] = invalid_name
+                data["name"] = invalid_name
 
                 serializer = CustomerSerializer(data=data)
 
                 self.assertFalse(serializer.is_valid())
-                self.assertCountEqual(serializer.errors, ['name'])
+                self.assertCountEqual(serializer.errors, ["name"])
 
         with self.subTest("Test email field content with invalid data."):
             data = serializer_data.copy()
@@ -80,12 +78,12 @@ class CustomerSerializerTestCase(TestCase):
             ]
 
             for invalid_email in invalid_emails:
-                data['email'] = invalid_email
+                data["email"] = invalid_email
 
                 serializer = CustomerSerializer(data=data)
 
                 self.assertFalse(serializer.is_valid())
-                self.assertCountEqual(serializer.errors, ['email'])
+                self.assertCountEqual(serializer.errors, ["email"])
 
         with self.subTest("Test phone field content with invalid data."):
             data = serializer_data.copy()
@@ -96,9 +94,9 @@ class CustomerSerializerTestCase(TestCase):
             ]
 
             for invalid_phone in invalid_phones:
-                data['phone'] = invalid_phone
+                data["phone"] = invalid_phone
 
                 serializer = CustomerSerializer(data=data)
 
                 self.assertFalse(serializer.is_valid())
-                self.assertCountEqual(serializer.errors, ['phone'])
+                self.assertCountEqual(serializer.errors, ["phone"])
