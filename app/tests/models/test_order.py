4f868f4de727164e2eb522a7e846c50751b3e6ae
Stévillis Sousa <stevillis@hotmail.com>
2022-11-06 23:57:13 -0400

    fix: implement relationship among Order, Product and OrderItem the right way

--- app/tests/models/test_order.py
+++ app/tests/models/test_order.py
@@ -6,7 +6,7 @@ from django.test import TransactionTestCase
 from django.utils import timezone
 from mixer.backend.django import mixer
 
-from app.models import Customer, Order, Product, ProductItem, Seller
+from app.models import Customer, Order, OrderItem, Product, Seller
 
 
 class OrderTestCase(TransactionTestCase):
@@ -20,10 +20,15 @@ class OrderTestCase(TransactionTestCase):
 
     def test_create_model_with_valid_data(self):
         """Create model with valid data should work as expected."""
-        datetime = timezone.now()
-
         customer = mixer.blend(Customer)
         seller = mixer.blend(Seller)
+        order = mixer.blend(
+            Order,
+            datetime=self.valid_datetime,
+            invoice_number='00001001',
+            customer=customer,
+            seller=seller
+        )
         prd1 = mixer.blend(
             Product,
             description="Alho",
@@ -36,34 +41,32 @@ class OrderTestCase(TransactionTestCase):
             unit_price=6.20,
             commission_percentage=0
         )
+
+        # Associate Products to the Order
         prd_items = [
             mixer.blend(
-                ProductItem,
+                OrderItem,
+                order=order,
                 product=prd1,
                 quantity=2
             ),
             mixer.blend(
-                ProductItem,
+                OrderItem,
+                order=order,
                 product=prd2,
                 quantity=1
             )
         ]
 
-        order = mixer.blend(
-            Order,
-            datetime=datetime,
-            invoice_number='00001001',
-            customer=customer,
-            seller=seller,
-        )
-        order.items.set(prd_items)
-
-        self.assertEqual(order.datetime, datetime)
+        self.assertEqual(order.datetime, self.valid_datetime)
         self.assertEqual(order.invoice_number, '00001001')
         self.assertEqual(order.customer, customer)
         self.assertEqual(order.seller, seller)
 
-        self.assertQuerysetEqual(order.items.all(), prd_items)
+        self.assertQuerysetEqual(
+            OrderItem.objects.filter(order=order),  # pylint: disable=no-member
+            prd_items
+        )
 
     def test_create_model_with_invalid_data(self):
         """Create model with valid data should work as expected."""
